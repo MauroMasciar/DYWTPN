95759df598c07ec2d6528ebfbb556637aebf7df4
Mauro Masciadro <mauro.masciar@hotmail.com>
2023-10-24 18:18:43 -0300

    first commit

---
+++ src/database/model.java
@@ -0,0 +1,22 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+public class model {
+    public static Connection conex;
+
+    public void connectDD() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conex = DriverManager.getConnection("jdbc:sqlite:database.db");
+            System.out.println("Conectado");
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println("Error en la conexión de la base de datos");
+            //JOptionPane.showMessageDialog(null, "Error en la conexión a la base de datos", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+}
---
+++ src/gui/MainWindow.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class MainWindow extends JFrame {
+	private JFrame j = new JFrame();
+	private JMenuBar menubar = new JMenuBar();
+	private JMenu mnuGames = new JMenu("Juegos");
+	private JMenuItem mnuiGamesRefresh = new JMenuItem("Actualizar");
+	public MainWindow() {
+		
+
+		j.setTitle("DYWTPN");
+		j.setSize(800, 600);
+		j.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		mnuGames.add(mnuiGamesRefresh);
+		menubar.add(mnuGames);
+		j.setJMenuBar(menubar);
+
+		j.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new MainWindow();
+	}
+}
